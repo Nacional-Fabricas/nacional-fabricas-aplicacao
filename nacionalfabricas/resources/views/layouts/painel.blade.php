6be867f75f8b739350d189604a06ff3c19cc01e1
lucifilho <lucielfilho@hotmail.com>
2025-02-12 13:35:28 -0300

    Finaliza estrutura do layout painel

--- nacionalfabricas/resources/views/layouts/painel.blade.php
+++ nacionalfabricas/resources/views/layouts/painel.blade.php
@@ -12,10 +12,10 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css" />
 
     <!-- Stylesheets -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css">
@@ -23,42 +23,38 @@
 
 </head>
 
-<body class="dashboardBody">
+<body class="layout-painel">
 
 <livewire:layout.sidebar />
 
-<div class="userArea">
-    <header>
-        <livewire:layout.header />
-    </header>
+<main class="main-painel">
 
-    <main>
-        <hr>
-        <div class="container">
-            @if(session('msg'))
-                <p class="msg">{{ session('msg') }}</p>
-            @elseif(session('sucesso'))
-                <p class="sucesso">{{ session('sucesso') }}</p>
-            @elseif(session('erro'))
-                <p class="erro">{{ session('erro') }}</p>
-            @endif
+    <hr>
+    @if(session('msg'))
+        <p class="msg">{{ session('msg') }}</p>
+    @elseif(session('sucesso'))
+        <p class="sucesso">{{ session('sucesso') }}</p>
+    @elseif(session('erro'))
+        <p class="erro">{{ session('erro') }}</p>
+    @endif
 
-            @yield('content')
-        </div>
-    </main>
-</div>
+    @yield('content')
+
+</main>
 
 @livewireScripts
 
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.6/jquery.inputmask.min.js"></script>
 <script src="https://js.pusher.com/8.0/pusher.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"></script>
+
 <script src="{{ asset('js/index.js') }}" type="module"></script>
+
 @stack('scripts')
 
 </body>

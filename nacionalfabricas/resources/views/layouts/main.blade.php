3bc9f300226060b19cfb1ac80eb64f9cd92871b2
lucifilho <lucielfilho@hotmail.com>
2025-02-25 15:40:10 -0300

    Adiciona o CDN do Tailwind

--- nacionalfabricas/resources/views/layouts/main.blade.php
+++ nacionalfabricas/resources/views/layouts/main.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     @livewireStyles
 </head>
 

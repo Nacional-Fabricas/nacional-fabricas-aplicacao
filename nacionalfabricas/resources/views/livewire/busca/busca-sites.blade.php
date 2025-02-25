cf19b78c3556441319d493c776e5de68c3c074c7
lucifilho <lucielfilho@hotmail.com>
2025-02-25 11:54:01 -0300

    Ajusta o Layout das setas de navegação dos slides

--- nacionalfabricas/resources/views/livewire/busca/busca-sites.blade.php
+++ nacionalfabricas/resources/views/livewire/busca/busca-sites.blade.php
@@ -24,7 +24,7 @@
 
                                 @if( $produto->id_conta == $site->id_conta )
 
-                                    <img class="swiper-slide" src="{{asset('storage/images/thumbnails/' .$produto->produto_thumbnail)}}">
+                                    <img class="swiper-slide" src="{{asset('storage/images/thumbnails/' . $produto-> produto_thumbnail)}} ">
 
                                 @endif
 

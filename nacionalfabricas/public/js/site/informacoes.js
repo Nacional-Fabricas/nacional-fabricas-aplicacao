5b48a85029982c70631e5a980c18e8fe092e6bf6
lucifilho <lucielfilho@hotmail.com>
2025-02-26 11:30:49 -0300

    Ajusta Layout das informações do site

--- nacionalfabricas/public/js/site/informacoes.js
+++ nacionalfabricas/public/js/site/informacoes.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const verInformacoesBtn = document.querySelector('.ver-informacoes');
     const informacoesContato = document.querySelector('.informacoes-contato');
-    const fecharInformacoesBtn = document.createElement('button');
-    fecharInformacoesBtn.classList.add('fechar-informacoes');
-    fecharInformacoesBtn.textContent = 'Fechar Informações';
-    informacoesContato.appendChild(fecharInformacoesBtn);
+    let fecharInformacoesBtn = document.querySelector('.fechar-informacoes');
+
+    if (!fecharInformacoesBtn) {
+        fecharInformacoesBtn = document.createElement('button');
+        fecharInformacoesBtn.classList.add('fechar-informacoes');
+        fecharInformacoesBtn.textContent = 'Fechar Informações';
+        informacoesContato.appendChild(fecharInformacoesBtn);
+    }
 
     verInformacoesBtn.addEventListener('click', () => {
         informacoesContato.style.display = 'flex';

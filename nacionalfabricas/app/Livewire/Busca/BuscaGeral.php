5bc92b2f52d745fadc8af832169d53fa70d109fe
lucifilho <lucielfilho@hotmail.com>
2025-02-26 08:23:32 -0300

    Ajusta a busca de sites e produtos, na busca Geral

--- nacionalfabricas/app/Livewire/Busca/BuscaGeral.php
+++ nacionalfabricas/app/Livewire/Busca/BuscaGeral.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Livewire\Busca;
 
+use App\Models\Cadastro;
 use App\Models\Estado;
 use App\Models\Produto;
 use App\Models\Segmento;
@@ -13,11 +14,11 @@ class BuscaGeral extends Component
 {
     use WithPagination;
 
-    public $tipo = 'Produtos';
+    public $tipo = 'Fábricas';
     public $busca = '';
     public $buscar;
-    public $estado = [];
-    public $segmento = [];
+    public $estado;
+    public $segmento = '';
     public $page = 1;
 
     protected $queryString = [
@@ -33,27 +34,57 @@ class BuscaGeral extends Component
     public function render()
     {
         try {
+
             $buscar = $this->buscar;
+            $estado = $this->estado;
             $tipo = $this->tipo;
             $estados = Estado::orderBy('sigla', 'asc')->get();
             $segmentos = Segmento::orderBy('nomeSegmento', 'asc')->get();
 
-            $produtos = Produto::where(function ($query) use ($buscar) {
-                $query->where('status', 'like', 'Ativo')
-                    ->where(function ($query) use ($buscar) {
-                        $query->where('produto_nome', 'like', '%' . $buscar . '%')
-                            ->orWhere('sku', 'like', '%' . $buscar . '%');
-                    });
-            })
-                ->orderBy('produto_nome')
-                ->orderBy('created_at')
-                ->paginate(4);
+            if ($tipo == "Produtos"){
 
-            $sites = Site::all();
+                $produtos = Produto::where(function ($query) use ($buscar) {
+                    $query->where('status', 'like', 'Ativo')
+                        ->where(function ($query) use ($buscar) {
+                            $query->where('produto_nome', 'like', '%' . $buscar . '%')
+                                ->orWhere('sku', 'like', '%' . $buscar . '%');
+                        });
+                })
+                    ->orderBy('produto_nome')
+                    ->orderBy('created_at')
+                    ->paginate(9);
+
+                if ( $estado){
+                    $produtos->where('estado', $this->estado);
+                }
+
+                $sites = Site::all();
+
+                $cadastros = Cadastro::all();
+
+            }elseif($tipo == "Fábricas"){
+
+                $sites = Site::where(function ($query) use ($buscar) {
+                    $query->where('status', 'like', 'Ativo')
+                        ->where(function ($query) use ($buscar) {
+                            $query->where('nome_industria', 'like', '%' . $buscar . '%')
+                                ->orWhere('produtos_tipo', 'like', '%' . $buscar . '%');
+                        });
+                })
+                    ->orderBy('nome_industria')
+                    ->orderBy('created_at')
+                    ->paginate(9);;
+
+                $produtos = Produto::where('status', 'Ativo')->where('destaque', 'Sim')->get();
+
+                $cadastros = Cadastro::all();
+
+            }
 
             return view('livewire.busca.busca-geral', [
                 'estado' => $this->estado,
                 'estados' => $estados,
+                'cadastros' => $cadastros,
                 'segmentos' => $segmentos,
                 'buscar' => $buscar,
                 'produtos' => $produtos,
--- nacionalfabricas/resources/views/livewire/busca/busca-geral.blade.php
+++ nacionalfabricas/resources/views/livewire/busca/busca-geral.blade.php
@@ -1,5 +1,7 @@
 <div>
+
     <div class="bloco-pesquisa">
+
         <div id="formPesquisa" class="formulario-pesquisa">
             <img id="close" onclick="pesquisaMobile()" src="{{ asset('icons/close.svg') }}" />
 
@@ -10,131 +12,37 @@
 
             <input wire:model.live="buscar" class="busca-texto" type="text" placeholder="O que está procurando?"/>
 
-            <select name="estado" class="escolha-estado" wire:model.defer="estado">
+            <select class="escolha-estado" wire:model.live="estado">
                 <option value="">Escolha um estado</option>
                 @foreach($estados as $estado)
                     <option value="{{ $estado->sigla }}">{{ $estado->sigla }}</option>
                 @endforeach
             </select>
 
-            <select name="segmento" class="escolha-segmento" wire:model.defer="segmento">
+            <select name="segmento" class="escolha-segmento" wire:model.live="segmento">
                 <option value="">Escolha um segmento</option>
                 @foreach($segmentos as $segmento)
                     <option value="{{ $segmento->codigo }}">{{ $segmento->nomeSegmento }}</option>
                 @endforeach
             </select>
+
         </div>
+
     </div>
 
     <div class="resultados">
+
         @if($tipo === "Produtos")
 
-            <div class="bloco-produtos">
-
-                <div class="contador-resultados">
-
-                    <strong>{{$produtos -> count()}}</strong> produtos encontrados
-
-                </div>
-
-                <ul class="lista-produtos">
-
-                    @foreach($produtos as $produto)
-
-                        <li class="produto">
-
-                            <div class="swiper mySwiper">
-
-                                <div class="swiper-wrapper">
-
-                                    <img class="swiper-slide" src="{{asset('storage/images/thumbnails/'. $produto -> produto_thumbnail) }}">
-
-                                </div>
-
-                                <div class="swiper-button-next"></div>
-                                <div class="swiper-button-prev"></div>
-
-                            </div>
-
-                            <div class="info">
-
-                                <h3>{{$produto -> produto_nome}}</h3>
-
-                                @if($produto -> preco_min != 0 && $produto -> preco_min != null && $produto -> preco_max != 0 && $produto -> preco_max != null )
-
-                                    <div class="preco">
-
-                                        <strong>R${{$produto -> preco_min}} - R${{$produto -> preco_max}}</strong>
-
-                                    </div>
-
-                                @endif
-
-                                <div class="codigos">
-
-                                    <span class="sku"><strong>SKU</strong> {{$produto -> sku}}</span>
-
-                                </div>
-
-                                <div class="acoes">
-
-                                    <a class="btn-ver-produto" href="{{ route('produto', [ 'id' => $produto -> id]) }}">Ver Produto</a>
-
-                                    @if($usuario && $produto -> id_conta != $usuario -> id )
-
-                                        <form action="{{route('adicionar_cotacao')}}" class="formulario-adicionar-cotacao"  method="POST" enctype="multipart/form-data">
-                                            @csrf
-
-                                            <input type="hidden" name="id" value="{{$produto -> id}}">
-                                            <input type="hidden" name="name" value="{{$produto -> produto_nome}}">
-                                            <input type="hidden" name="sku" value="{{$produto -> sku}}">
-                                            <input type="hidden" name="ean" value="{{$produto -> ean}}">
-                                            <input type="hidden" name="id_receptor" value="{{$produto -> id_conta}}">
-                                            <input type="hidden" name="id_produto" value="{{$produto -> id}}">
-                                            <input type="hidden" name="price" value="1">
-                                            <input type="hidden" name="quantity" value="1">
-                                            <input type="hidden" name="image" value="{{$produto-> produto_thumbnail}}">
-
-                                            <button class="addCote full">+ Adicionar cotação</button>
-
-                                        </form>
-
-                                    @else
-
-                                        <a class="btn-editar-produto" href="{{ route('editar_produto', [ 'id' => $produto -> id])}}">Editar Produto</a>
-
-                                    @endif
-
-                                    @foreach($sites as $site)
-
-                                        @if($site -> id == $produto -> id_site )
-
-                                            <a class="btn-ver-fabricante" href="{{ route('site', [ 'id' => $produto -> id_site, 'slug' => $site -> slug])}}">Ver Fabricante</a>
-
-                                        @endif
-
-                                    @endforeach
-
-                                </div>
-
-                            </div>
-
-                        </li>
-
-                    @endforeach
-
-                </ul>
-
-                <div class="paginacao">
-                    {{$produtos->appends(request()->query())->links()}}
-                </div>
-
-            </div>
+            @include('pages.buscas.busca-produtos')
 
         @elseif($tipo === "Fábricas")
-            <livewire:busca.busca-sites :buscar="$buscar" />
+
+            @include('pages.buscas.busca-site')
+
         @endif
 
         <livewire:gerais.mapa-busca />
+
     </div>
 </div>

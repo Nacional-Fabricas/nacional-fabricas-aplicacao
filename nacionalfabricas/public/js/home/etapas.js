a789e0fb6c7717e21eae131f98e6b153f8c8652e
lucifilho <lucielfilho@hotmail.com>
2025-02-27 16:41:00 -0300

    Define animação das etapas pelo SCroll Position

--- nacionalfabricas/public/js/home/etapas.js
+++ nacionalfabricas/public/js/home/etapas.js
@@ -1,13 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     const etapas = document.querySelectorAll('.etapa');
-    etapas.forEach((etapa, index) => {
-        setTimeout(() => {
-            const numero = etapa.querySelector('.etapa-numero');
-            const descricao = etapa.querySelector('.etapa-descricao');
-            numero.classList.add('show');
+    let animationStarted = false;
+
+    function startAnimation() {
+        etapas.forEach((etapa, index) => {
             setTimeout(() => {
-                descricao.classList.add('show');
-            }, 500); // Delay between numero and descricao
-        }, (index + 1) * 1500); // Delay between each etapa
+                const numero = etapa.querySelector('.etapa-numero');
+                const descricao = etapa.querySelector('.etapa-descricao');
+                numero.classList.add('show');
+                setTimeout(() => {
+                    descricao.classList.add('show');
+                }, 500); // Delay between numero and descricao
+            }, (index + 1) * 1500); // Delay between each etapa
+        });
+    }
+
+    window.addEventListener('scroll', function () {
+
+        if (!animationStarted && window.scrollY >= 150) {
+            animationStarted = true;
+            startAnimation();
+        }
     });
 });

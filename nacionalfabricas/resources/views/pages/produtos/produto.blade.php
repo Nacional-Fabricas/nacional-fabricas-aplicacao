ed57118e2407285ee160dbbb833ae59040fc81f3
lucifilho <lucielfilho@hotmail.com>
2025-02-27 16:16:00 -0300

    Ajusta o layout da busca da página do produto

--- nacionalfabricas/resources/views/pages/produtos/produto.blade.php
+++ nacionalfabricas/resources/views/pages/produtos/produto.blade.php
@@ -1,124 +1,124 @@
 @extends('layouts.meu-site')
 @section('content')
 
-    <img class="banner" src="{{asset('storage/images/sites/banners/'. $site -> banner)}}">
+    <div class="pagina-produto">
 
-    <div class="informacao-site">
+        <img class="banner" src="{{asset('storage/images/sites/banners/'. $site -> banner)}}">
 
-        <img class="logo" src="{{ asset('storage/images/sites/logos/' . $site -> logo)}}" alt="{{$site -> nome_industria}}">
+        <div class="informacao-site">
 
-        <div class="bloco-informacoes">
+            <img class="logo" src="{{ asset('storage/images/sites/logos/' . $site -> logo)}}" alt="{{$site -> nome_industria}}">
 
-            <div class="cabecalho">
+            <div class="bloco-informacoes">
 
-                <h1>{{$site -> nome_industria}}</h1>
+                <div class="cabecalho">
 
-                <div class="informacoes-basica">
+                    <h1>{{$site -> nome_industria}}</h1>
 
-                    <h3>Informações básicas</h3>
+                    <div class="informacoes-basica">
 
-                    <span class="descricao">{{$site -> descricao_industria}}</span>
+                        <h3>Informações básicas</h3>
+
+                        <span class="descricao">{{$site -> descricao_industria}}</span>
+
+                    </div>
 
                 </div>
 
-            </div>
+                <button class="ver-informacoes">Ver Informações</button>
 
-            <button class="ver-informacoes">Ver Informações</button>
+                <ul class="informacoes-contato">
 
-            <ul class="informacoes-contato">
+                    <img id="close" onclick="verInfo()" src="{{ asset('icons/close.svg')}}" />
 
-                <img id="close" onclick="verInfo()" src="{{ asset('icons/close.svg')}}" />
+                    <li class="informacao-contato">
 
-                <li class="informacao-contato">
+                        <div class="sobre-info">
 
-                    <div class="sobre-info">
+                            <img class="icone" src="{{ asset('icons/location.svg')}}" alt="">
+                            <h4>Endereço</h4>
 
-                        <img class="icone" src="{{ asset('icons/location.svg')}}" alt="">
-                        <h4>Endereço</h4>
+                        </div>
 
-                    </div>
+                        <div class="conteudo-destaque">
 
-                    <div class="conteudo-destaque">
+                                <span class="valor-destaque">
+
+                                    {{$endereco -> endereco}}, {{$endereco -> bairro}},{{$endereco -> numero}}, {{$endereco -> cidade}} - {{$endereco -> estado}}
+
+                                </span>
+
+                        </div>
+
+                    </li>
+
+                    <li class="informacao-contato">
+
+                        <div class="sobre-info">
+
+                            <img src="{{ asset('icons/call.svg')}}" alt="">
+                            <h4>Contato</h4>
+
+                        </div>
+
+                        <div class="conteudo-destaque">
 
                             <span class="valor-destaque">
 
-                                {{$endereco -> endereco}}, {{$endereco -> bairro}},{{$endereco -> numero}}, {{$endereco -> cidade}} - {{$endereco -> estado}}
+                                Email: {{$site -> email}}
 
                             </span>
 
-                    </div>
+                            <span class="valor-destaque">
 
-                </li>
+                                Telefone: {{$site -> telefone}}
 
-                <li class="informacao-contato">
+                            </span>
 
-                    <div class="sobre-info">
+                        </div>
 
-                        <img src="{{ asset('icons/call.svg')}}" alt="">
-                        <h4>Contato</h4>
+                    </li>
 
-                    </div>
+                    <li class="informacao-contato">
 
-                    <div class="conteudo-destaque">
+                        <div class="sobre-info">
 
-                        <span class="valor-destaque">
+                            <img src="{{ asset('icons/stopwatch.svg')}}" alt="">
+                            <h4>Atendimento</h4>
+                        </div>
 
-                            Email: {{$site -> email}}
+                        <div class="conteudo-destaque">
 
-                        </span>
+                            <a class="ver-horarios" onclick="verHorarios()">Ver Horários</a>
 
-                        <span class="valor-destaque">
+                            <div class="bloco-horarios" style="display: none">
 
-                            Telefone: {{$site -> telefone}}
+                                <div class="todos-horarios">
 
-                        </span>
+                                    <h3>horários de atendimento</h3>
 
-                    </div>
+                                    @foreach($atendimento as $dia => $info)
+                                        @if(isset($info['entrada']) && $info['entrada'] !== 'N/A' && isset($info['saida']) && $info['saida'] !== 'N/A')
+                                            <p class="horario"><span class="dia-semana">{{ ucfirst($dia) }}:</span> <span class="horario-semana">{{ $info['entrada'] }}h - {{ $info['saida'] }}h</span></p>
+                                        @endif
+                                    @endforeach
 
-                </li>
+                                    <a class="ver-horarios" onclick="verHorarios()">Fechar</a>
 
-                <li class="informacao-contato">
-
-                    <div class="sobre-info">
-
-                        <img src="{{ asset('icons/stopwatch.svg')}}" alt="">
-                        <h4>Atendimento</h4>
-                    </div>
-
-                    <div class="conteudo-destaque">
-
-                        <a class="ver-horarios" onclick="verHorarios()">Ver Horários</a>
-
-                        <div class="bloco-horarios" style="display: none">
-
-                            <div class="todos-horarios">
-
-                                <h3>horários de atendimento</h3>
-
-                                @foreach($atendimento as $dia => $info)
-                                    @if(isset($info['entrada']) && $info['entrada'] !== 'N/A' && isset($info['saida']) && $info['saida'] !== 'N/A')
-                                        <p class="horario"><span class="dia-semana">{{ ucfirst($dia) }}:</span> <span class="horario-semana">{{ $info['entrada'] }}h - {{ $info['saida'] }}h</span></p>
-                                    @endif
-                                @endforeach
-
-                                <a class="ver-horarios" onclick="verHorarios()">Fechar</a>
+                                </div>
 
                             </div>
 
                         </div>
 
-                    </div>
+                    </li>
 
-                </li>
+                </ul>
 
-            </ul>
+            </div>
+
 
         </div>
-
-
-    </div>
-
-    <div class="pagina-produto">
 
         <div class="cabecalho-produto">
 
